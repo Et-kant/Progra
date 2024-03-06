b2cfcec735839e23989ab2e483a746db444094fa
Et-kant <gilrestrepoemanuel@gmail.com>
2024-03-05 20:27:45 -0500

    chng-ex13

--- src/Ejercicios/Exercise_13.java
+++ src/Ejercicios/Exercise_13.java
@@ -1,12 +1,12 @@
 package Ejercicios;
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Exercise_13 {
     public static void main(String[] args){
         Scanner keyboard = new Scanner(System.in);
-        int big = 0;
-
         int [] Array1 = new int[4];
+        int big = Array1[0];
 
         for (int i = 0; i < Array1.length; i++) {
             System.out.println("Please enter the values for the Array: ");
@@ -14,10 +14,8 @@ public class Exercise_13 {
         }
 
         for (int j = 0; j < Array1.length; j++) {
-            for (int k = 1; k < Array1.length; k++) {
-                if (Array1[j] > Array1[k - 1]) {
-                    big = Array1[j];
-                }
+            if (Array1[j] > big){
+                big = Array1[j];
             }
         }
 
